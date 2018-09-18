4fc62737c57277957136deb5334a076af89c92fc
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-09-18 11:35:48 +0700

    ajout map

--- assets/js/mapbox.js
+++ assets/js/mapbox.js
@@ -1,6 +1,6 @@
   var upstairsLab = [102.6216070, 17.9386939];
 
-  mapboxgl.accessToken = 'pk.eyJ1Ijoibmljb2xhc2R1cXVlc25lIiwiYSI6ImNqaTMyejI3ejE2dnUzdmxrdXBpMW5tM3MifQ.qeLFoY_OigVRg_GY5mul7A';
+  mapboxgl.accessToken = 'pk.eyJ1Ijoibmljb2xhc2R1cXVlc25lIiwiYSI6ImNqbTc3enk3ajQ1ejAzcGxpdW9xaGdxZGQifQ.A4Z8N0v-t3TurNZmIgTmHA';
   var map_box = new mapboxgl.Map({
     container: 'map_box',
     center: upstairsLab,
